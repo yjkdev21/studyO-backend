1f4e67a5e74821757975af76ad1a27dbefac96cc
rlaksl <eehiuh@gmail.com>
2025-07-31 14:40:10 +0900

    7/31 사이드바 작업

---
+++ src/main/java/com/ex/tjspring/study/dao/SidebarDao.java
@@ -0,0 +1,19 @@
+package com.ex.tjspring.study.dao;
+
+import com.ex.tjspring.study.dto.SidebarDto;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SidebarDao {
+    /**
+     * 스터디 ID로 사이드바에 필요한 스터디 정보 조회
+     * @param studyId: 조회할 스터디의 고유 식별자(study_groups.id)
+     * @return SidebarDto: 사이드바 표시용 스터디 정보(id, category, name, contact)
+     * 해당 ID의 스터디가 없으면 null 값 반환
+     * XML MAPPER 파일의 <select id='getStudyInfo /> 와 매핑됨
+     * SQL: SELECT id, category, group_name as name, contact
+     *      FROM study_groups where id = #{studyId}
+    **/
+    SidebarDto getStydyInfo(Long studyId);
+
+}
---
+++ src/main/java/com/ex/tjspring/study/service/SidebarServiceImpl.java
@@ -0,0 +1,17 @@
+package com.ex.tjspring.study.service;
+
+import com.ex.tjspring.study.dao.SidebarDao;
+import com.ex.tjspring.study.dto.SidebarDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SidebarServiceImpl {
+    @Autowired
+    private SidebarDao sidebarDao;
+
+    public SidebarDto getStudyInfo(Long studyId) {
+        return sidebarDao.getStudyInfo(studyId);
+    }
+
+}
