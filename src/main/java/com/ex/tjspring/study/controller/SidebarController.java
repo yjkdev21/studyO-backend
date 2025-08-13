42c8dacc1ca01481b16771e7a732b52a94682023
rlaksl <eehiuh@gmail.com>
2025-08-13 12:02:20 +0900

    스터디 대시보드 통합 API 추가

--- src/main/java/com/ex/tjspring/study/controller/SidebarController.java
+++ src/main/java/com/ex/tjspring/study/controller/SidebarController.java
@@ -3,6 +3,7 @@ package com.ex.tjspring.study.controller;
 import com.ex.tjspring.study.dto.SidebarDto;
 import com.ex.tjspring.study.service.SidebarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,15 @@ public class SidebarController {
     private SidebarServiceImpl sidebarService;
 
     @GetMapping("/study/{groupId}")
-    public SidebarDto getStudyInfo(@PathVariable Long groupId) {
-        return sidebarService.getStudyInfo(groupId);
+    public ResponseEntity<?> getStudyInfo(@PathVariable Long groupId) {
+        SidebarDto dto = sidebarService.getStudyInfo(groupId);
+
+        if (dto == null) {
+            return ResponseEntity
+                    .status(404)
+                    .body("해당 스터디(" + groupId + ")는 존재하지 않습니다.");
+        }
+
+        return ResponseEntity.ok(dto);
     }
 }
