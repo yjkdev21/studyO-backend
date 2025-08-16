9e37526e7230fc2b33a3c943445a2f03215b06a5
yuseon4455 <yuseon0344@naver.com>
2025-08-16 09:29:43 +0900

    코드수정

---
+++ src/main/java/com/ex/tjspring/admin/dto/AdminBoardDto.java
@@ -0,0 +1,13 @@
+package com.ex.tjspring.admin.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AdminBoardDto {
+
+    private String region;
+}
